5503331b224695eedd3db4fdd429ba6d1b0ea967
Jim Publicover <jameslpublicover@gmail.com>
2017-04-21 13:17:37 -0400

    set dice values

--- lib/die.rb
+++ lib/die.rb
@@ -1,3 +1,76 @@
-class Die < Array
+class Die
+
+  def die_1
+    ["I", "E", "E", "S", "U", "N"]
+  end
+
+  def die_2
+    ["P", "F", "K", "F", "A", "S"]
+  end
+
+  def die_3
+    ["E", "H", "W", "G*", "N", "E"]
+  end
+
+  def die_4
+    ["V", "E*", "T", "H", "W", "R"]
+  end
+
+  def die_5
+    ["D", "L*", "R", "X", "I", "E"]
+  end
+
+  def die_6
+    ["D", "I", "T", "Y", "S", "T"]
+  end
+
+  def die_7
+    ["O", "H", "S", "C", "A", "P"]
+  end
+
+  def die_8
+    ["V", "E", "Y", "D", "L", "R"]
+  end
+
+  def die_9
+    ["T", "L", "E", "T", "R", "Y"]
+  end
+
+  def die_10
+    ["E", "E", "A", "N", "A", "G*"]
+  end
+
+  def die_11
+    ["J", "O", "B*", "B", "O", "A"]
+  end
+
+  def die_12
+    ["T", "I", "O", "S", "E", "S"]
+  end
+
+  def die_13
+    ["H", "N", "Qu", "M", "U", "I"]
+  end
+
+  def die_14
+    ["Z", "N", "L", "R", "N", "H"]
+  end
+
+  def die_15
+    ["M", "I", "C", "O", "U", "T"]
+  end
+
+  def die_16
+    ["T", "O*", "W", "O", "A", "T"]
+  end
+
+  def before_shake
+    [die_1, die_2, die_3, die_4, die_5, die_6, die_7, die_8, die_9, die_10,
+    die_11, die_12, die_13, die_14, die_15, die_16]
+  end
+
+  def after_shake
+
+  end
 
 end
